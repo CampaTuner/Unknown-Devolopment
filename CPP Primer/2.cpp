bc14ce12ff21df3b7b8bb3744213a20e12b470ab
Rounak <rounakbasak.personal@gmail.com>
2025-11-01 02:58:03 +0530

    add more

---
+++ CPP Primer/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main()
+{
+    int sum = 0, val = 1;
+    // keep executing the while as long as val is less than or equal to 10
+    
+    /* 
+        while (val <= 10)
+        {
+            sum += val; // assigns sum + val to sum
+            ++val;      // add 1 to val
+        }
+        std::cout << "Sum of 1 to 10 inclusive is "
+                << sum << std::endl;
+    */
+
+    for (int val = 1; val <= 10; ++val)
+        sum += val; // equivalent to sum = sum + val
+    std::cout << "Sum of 1 to 10 inclusive is "
+              << sum << std::endl;
+    return 0;
+}
